f1a09861b283221f013d9fb7f15c26d050daa3e6
Atul Kumbhar <atul.kumbhar1985@gmail.com>
2021-08-12 08:37:37 +0000

    First Commit

---
+++ src/main/java/com/simplilearn/CMTAssessPractice/CMTAssessPractice/CmtAssessPracticeApplication.java
@@ -0,0 +1,21 @@
+  
+package com.SimplilearnJenkinsAssess.JenkinsAssess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsAssessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsAssessApplication.class, args);
+	}
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("<h1>Hello %s!</h1> <h2>This is sample header</h2>", name);
+	}
+}
---
+++ src/test/java/com/simplilearn/CMTAssessPractice/CMTAssessPractice/CmtAssessPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.CMTAssessPractice.CMTAssessPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmtAssessPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
